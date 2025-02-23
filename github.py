9e26f58b7a4771c8bae8dade572b72665ba5a4f6
Emil09876 <emilbruhanov6@gmail.com>
2025-02-23 13:55:14 +0300

    Add files via upload

---
+++ github.py
@@ -0,0 +1,11 @@
+number_1 = int(input('Введите первое число: '))
+number_2 = int(input('Введите второе число: '))
+x = input('Выберите операцию: ')
+
+if x == '+':
+    a = number_1 + number_2
+    print(a)
+elif x == '-':
+    b = number_1 - number_2
+    
+elif x
