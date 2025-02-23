9ad9daa77305b02822b2c08c3dbf0207919505f5
Emil09876 <emilbruhanov6@gmail.com>
2025-02-23 14:08:32 +0300

    Add files via upload

--- github.py
+++ github.py
@@ -1,11 +1,16 @@
 number_1 = int(input('Введите первое число: '))
 number_2 = int(input('Введите второе число: '))
-x = input('Выберите операцию: ')
+x = input('Выберите операцию( + , - , * , / ): ')
 
 if x == '+':
     a = number_1 + number_2
-    print(a)
+    print(f'Результат {x} чисел {number_1},{number_2} равняется >>{a}<<')
 elif x == '-':
     b = number_1 - number_2
-    
-elif x
+    print(f'Результат {x} чисел {number_1},{number_2} равняется >>{b}<<')
+elif x == '*':
+    c = number_1 * number_2
+    print(f'Результат {x} чисел {number_1},{number_2} равняется >>{c}<<')
+else:
+    d = number_1 / number_2
+    print(f'Результат {x} чисел {number_1},{number_2} равняется >>{d}<<')
